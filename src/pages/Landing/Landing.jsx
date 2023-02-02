ed23a1f3736e8716e118692f8e6515603e504bcd
DanielleColucci <ddcolucci99@gmail.com>
2023-02-02 15:21:25 -0500

    update David's Things Link route

--- src/pages/Landing/Landing.jsx
+++ src/pages/Landing/Landing.jsx
@@ -6,7 +6,7 @@ const Landing = () => {
       {/* All the <Link> components should live here */}
       <h1>All-The-Things</h1>
       <Link to="/the-manliest-things">Ben's Things</Link><br/>
-      <Link to="/the-well-styled-things">David's Things</Link><br/>
+      <Link to="/the-okay-ish-styled-things">David's Things</Link><br/>
       <Link to="/the-silly-things">Hunter's Things</Link><br/>
       <Link to="/the-nerdy-things">Patrick's THings</Link><br/>
     </>
