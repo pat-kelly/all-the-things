68ad21ae5f03d5f5bb93eea97c2e9feda71584ee
Mary Lueder <mlueder@gmail.com>
2023-02-02 15:20:49 -0500

    rosebud

--- src/pages/NatureThings/NatureThings.jsx
+++ src/pages/NatureThings/NatureThings.jsx
@@ -1,9 +1,11 @@
 import ThingCard from "../../components/ThingCard/ThingCard";
+import { Link } from "react-router-dom";
 
 const NatureThings = ({things}) => {
   return (  
     <>
       <h3>Mary's Nature Things</h3>
+      <Link to='/'>Home</Link>
       {things.map(thing => (
         <ThingCard thing={thing}/>
       ))}
